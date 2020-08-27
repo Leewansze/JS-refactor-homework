8f9c78ddffe9aa1b3d158c63e928fe404ec7a926
Leewansze <1226186467@qq.com>
2020-08-28 00:18:18 +0800

    [JS-refactor-homework][alice] refactor: delete duplicate code

--- test/rankTest.js
+++ test/rankTest.js
@@ -10,6 +10,31 @@ const { captainHistoryRisk } = require('../src/rank')
 //   t.is(await bar, 'bar');
 // });
 
+const voyageHasChina = {
+    zone: 'china',
+    length:  10,
+};
+
+const historyLengtOverFive = [{
+    zone: 'east-indies',
+    profit: 5,
+}, {
+    zone: 'west-indies',
+    profit: 15,
+}, {
+    zone: 'china',
+    profit: -2,
+}, {
+    zone: 'china',
+    profit: -2,
+}, {
+    zone: 'china',
+    profit: -2,
+}, {
+    zone: 'west-africa',
+    profit: 7,
+}, ];
+
 rankTest('should return 7 when voyageRisk given voyage length > 8', t => {
     //given
     const voyage = {
@@ -71,23 +96,13 @@ rankTest('should return 7 when voyageRisk given voyage zone is china and voyage 
 })
 
 rankTest('should return 9 when voyageRisk given voyage zone is china and voyage length > 8', t => {
-    //given
-    const voyage = {
-        zone: 'china',
-        length:  10,
-    };
     //when
-    const result = voyageRisk(voyage)
+    const result = voyageRisk(voyageHasChina)
         //then
     t.is(result, 9)
 })
 
 rankTest('should return 4 when captainHistoryRisk given voyage zone is china and history length < 5', t => {
-    //given
-    const voyage = {
-        zone: 'china',
-        length:  10,
-    };
     const history = [{
         zone: 'east-indies',
         profit: 5,
@@ -102,38 +117,14 @@ rankTest('should return 4 when captainHistoryRisk given voyage zone is china and
         profit: 7,
     }, ];
     //when
-    const result = captainHistoryRisk(voyage, history)
+    const result = captainHistoryRisk(voyageHasChina, history)
         //then
     t.is(result, 4)
 })
 
 rankTest('should return 2 when captainHistoryRisk given voyage zone is china and history length > 5', t => {
-    //given
-    const voyage = {
-        zone: 'china',
-        length:  10,
-    };
-    const history = [{
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'west-africa',
-        profit: 7,
-    }, ];
     //when
-    const result = captainHistoryRisk(voyage, history)
+    const result = captainHistoryRisk(voyageHasChina, historyLengtOverFive)
         //then
     t.is(result, 2)
 })
@@ -144,27 +135,8 @@ rankTest('should return 4 when captainHistoryRisk given voyage zone not china an
         zone: 'east-indies',
         length:  10,
     };
-    const history = [{
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'china',
-        profit: -2,
-    }, {
-        zone: 'west-africa',
-        profit: 7,
-    }, ];
     //when
-    const result = captainHistoryRisk(voyage, history)
+    const result = captainHistoryRisk(voyage, historyLengtOverFive)
         //then
     t.is(result, 4)
 })
