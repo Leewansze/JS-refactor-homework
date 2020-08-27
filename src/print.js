dd57fdd1b652ebf3968761e3cd46efee7bfe0a1b
Leewansze <1226186467@qq.com>
2020-08-28 04:26:00 +0800

    [JS-refactor-homework][alice] refactor: extract get text result method

--- src/print.js
+++ src/print.js
@@ -1,8 +1,6 @@
 function printOwing(invoice) {
     let outstanding = 0;
-    let result = `***********************\n` +
-        `**** Customer Owes ****\n` +
-        `***********************\n`;
+
     // console.log('***********************');
     // console.log('**** Customer Owes ****');
     // console.log('***********************');
@@ -17,9 +15,7 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
     // print details
-    result += `name: ${invoice.customer}\n`;
-    result += `amount: ${outstanding}\n`;
-    result += `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
+    let result = getTxtResult(invoice, outstanding);
     // console.log(`name: ${invoice.customer}`);
     // console.log(`amount: ${outstanding}`);
     // console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
@@ -28,4 +24,14 @@ function printOwing(invoice) {
 
 module.exports = {
     printOwing
+}
+
+function getTxtResult(invoice, outstanding) {
+    let result = `***********************\n` +
+        `**** Customer Owes ****\n` +
+        `***********************\n`;
+    result += `name: ${invoice.customer}\n`;
+    result += `amount: ${outstanding}\n`;
+    result += `amount: ${invoice.dueDate.toLocaleDateString()}\n`;
+    return result;
 }
