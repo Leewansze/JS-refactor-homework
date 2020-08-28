722410f00b431e3e27b39b6f7169aa41affce6f1
Leewansze <1226186467@qq.com>
2020-08-28 21:09:07 +0800

    [JS-refactor-homework][alice] test: should return date for delay 3 days when deliveryDate given deliveryState has NY and isrush is true

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -1,7 +1,7 @@
 const deliveryTest = require('ava');
 const {deliveryDate} = require("../src/delivery")
 
-deliveryTest('should return date for delay 2 days when deliveryDate given abOrder and isrush is true', t => {
+deliveryTest('should return date for delay 2 days when deliveryDate given deliveryState has ct and isrush is true', t => {
   //given
   const isRush = true;
   const anOrder = {
@@ -15,6 +15,28 @@ deliveryTest('should return date for delay 2 days when deliveryDate given abOrde
     }
   }
   let delayDay = 2;
+  let deliveryDay = new Date();
+  deliveryDay.setDate(deliveryDay.getDate() + delayDay)
+  //when
+  const result = deliveryDate(anOrder, isRush)
+  //then
+  t.is(result, deliveryDay.toLocaleDateString())
+})
+
+deliveryTest('should return date for delay 3 days when deliveryDate given deliveryState has NY and isrush is true', t => {
+  //given
+  const isRush = true;
+  const anOrder = {
+    deliveryState: 'NY',
+    placedOn: {
+      plusDays(delivery) {
+        let realDelivery = new Date();
+        realDelivery.setDate(realDelivery.getDate() + delivery)
+        return realDelivery.toLocaleDateString();
+      }
+    }
+  }
+  let delayDay = 3;
   let deliveryDay = new Date();
   deliveryDay.setDate(deliveryDay.getDate() + delayDay)
   //when
