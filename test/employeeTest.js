de9f2bf4d76922279881e3cb84bd442e4c1f6abe
Leewansze <1226186467@qq.com>
2020-08-28 11:42:23 +0800

    [JS-refactor-homework][alice] test:should return not throw exception when new a employee given alice and engineer

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -9,3 +9,8 @@ employeeTest('shoule return name and type when tostring given alice and engineer
     //then
     t.is(result, `Alice (engineer)`)
 })
+
+employeeTest('should return not throw exception when new a employee given alice and engineer', t => {
+    const employee = new Employee('Alice', 'engineer')
+    t.notThrows(() => employee)
+})
