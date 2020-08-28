3a951ffa236d4d6711bc42a9785de0ece26a303d
Leewansze <1226186467@qq.com>
2020-08-28 21:13:44 +0800

    [JS-refactor-homework][alice] test: should return date for delay 4 days when deliveryDate given deliveryState unqualified and isrush is true

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -44,3 +44,25 @@ deliveryTest('should return date for delay 3 days when deliveryDate given delive
   //then
   t.is(result, deliveryDay.toLocaleDateString())
 })
+
+deliveryTest('should return date for delay 4 days when deliveryDate given deliveryState unqualified and isrush is true', t => {
+  //given
+  const isRush = true;
+  const anOrder = {
+    deliveryState: 'AB',
+    placedOn: {
+      plusDays(delivery) {
+        let realDelivery = new Date();
+        realDelivery.setDate(realDelivery.getDate() + delivery)
+        return realDelivery.toLocaleDateString();
+      }
+    }
+  }
+  let delayDay = 4;
+  let deliveryDay = new Date();
+  deliveryDay.setDate(deliveryDay.getDate() + delayDay)
+  //when
+  const result = deliveryDate(anOrder, isRush)
+  //then
+  t.is(result, deliveryDay.toLocaleDateString())
+})
