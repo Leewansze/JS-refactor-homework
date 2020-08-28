948dc22b01c42cb09e2bee4d835214422c264652
Leewansze <1226186467@qq.com>
2020-08-28 21:50:59 +0800

    [JS-refactor-homework][alice] refactor: extract the method of deliveryDate with rush

--- src/delivery.js
+++ src/delivery.js
@@ -1,38 +1,41 @@
+function deliveryWithTrueRush(anOrder) {
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  } else if ([
+    'NY',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else {
+    deliveryTime = 3;
+  }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function deliveryWithFalseRush(anOrder) {
+  let deliveryTime;
+  if ([
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
 function deliveryDate(anOrder, isRush) {
-    if (isRush) {
-        let deliveryTime;
-        if ([
-                'MA',
-                'CT',
-            ].includes(anOrder.deliveryState)) {
-            deliveryTime = 1;
-        } else if ([
-                'NY',
-                'NH',
-            ].includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 3;
-        }
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
-    } else {
-        let deliveryTime;
-        if ([
-                'MA',
-                'CT',
-                'NY',
-            ].includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else if ([
-                'ME',
-                'NH',
-            ].includes(anOrder.deliveryState)) {
-            deliveryTime = 3;
-        } else {
-            deliveryTime = 4;
-        }
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
-    }
+    return isRush ? deliveryWithTrueRush(anOrder): deliveryWithFalseRush(anOrder);
 }
 
 module.exports = {
